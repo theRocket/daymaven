a8d605cf8d0ca71faa27e15545aaac9705f56362
Ryan Rickerts <ryan@rocketdesigns.net>
2011-03-05 20:12:50 -0800

    Initialize Rails app with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daymaven::Application.config.secret_token = '8775c6ba534ac7a3955b6769a3c85d66d443e510b6b07a77d648f5ffac2cccefa1fe8a557d1938ca5c18f6bf95c63ced2df3acf2585bb947f40799b5020881b8'
