97a7262103624d4ba604b0f5d88055d88ee6cdbe
Ryan Rickerts <ryan@rocketdesigns.net>
2011-03-05 23:37:48 -0800

    Days need a better controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,8 +1,93 @@
 class DaysController < ApplicationController
-  def index
-  end
+  helper :all
 
-  def show
-  end
+  before_filter :authenticate, :except => [:index,:show]
 
+    # GET /days
+    # GET /days.xml
+    def index
+  	@days = Day.search(params[:search])
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @days }
+      end
+    end
+
+    # GET /days/1
+    # GET /days/1.xml
+    def show
+      @day = Day.find(params[:id])
+
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @day }
+      end
+    end
+
+    # GET /days/new
+    # GET /days/new.xml
+    def new
+      @day = Day.new
+
+       @user = User.find(current_user);
+  	    @day.user_id = @user.id
+
+      respond_to do |format|
+        format.html # new.html.erb
+        format.xml  { render :xml => @day }
+      end
+    end
+
+    # GET /days/1/edit
+    def edit
+      @day = Day.find(params[:id])
+    end
+
+    # POST /days
+    # POST /days.xml
+    def create
+      @day = Day.new(params[:day])
+        @user = User.find(current_user);
+  			    @day.user_id = @user.id
+
+
+      respond_to do |format|
+        if @day.save
+          format.html { redirect_to(@day, :notice => 'Day was successfully created.') }
+          format.xml  { render :xml => @day, :status => :created, :location => @day }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @day.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+
+    # PUT /days/1
+    # PUT /days/1.xml
+    def update
+      @day = Day.find(params[:id])
+
+      respond_to do |format|
+        if @day.update_attributes(params[:day])
+          format.html { redirect_to(@day, :notice => 'Day was successfully updated.') }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @day.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+
+    # DELETE /days/1
+    # DELETE /days/1.xml
+    def destroy
+      @day = Day.find(params[:id])
+      @day.destroy
+
+      respond_to do |format|
+        format.html { redirect_to(days_url) }
+        format.xml  { head :ok }
+      end
+    end
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,11 @@
 class Day < ActiveRecord::Base
   belongs_to :user
+  
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['description LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
