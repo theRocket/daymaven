3da653d8d3c662c30e0591bf51d2607218bc8452
Ryan Rickerts <ryan@rocketdesigns.net>
2011-03-06 00:02:53 -0800

    Add search to Day actions excluded from authentication

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,7 +1,7 @@
 class DaysController < ApplicationController
   helper :all
 
-  before_filter :authenticate_user!, :except => [:index,:show]
+  before_filter :authenticate_user!, :except => [:index,:show, :search]
 
     # GET /days
     # GET /days.xml
