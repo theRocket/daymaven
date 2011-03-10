60e585ea4539116dc7a9590cf71b9ee77cd2e760
Ryan Rickerts <ryan@rocketdesigns.net>
2011-03-09 17:50:58 -0800

    Only displaying remaining available auth providers to User.

--- app/controllers/users/authentications_controller.rb
+++ app/controllers/users/authentications_controller.rb
@@ -1,6 +1,12 @@
 class Users::AuthenticationsController < ApplicationController
   def index
     @authentications = current_user.authentications if current_user
+    already_authed = []
+    @authentications.each do |auth|
+      already_authed << auth.provider
+    end
+    providers = User.omniauth_providers.map {|oa| oa.to_s }
+    @providers_noauth = providers - already_authed
   end
   
 end
