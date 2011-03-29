2ce54e7d06a2a80b0474c8335a8a80fbb66346eb
Jacob Hartsoch <jacobhartsoch@yahoo.com>
2011-03-29 15:01:02 -0700

    cleaned up search and added city support (will consider a move to solr at some point)

--- app/models/day.rb
+++ app/models/day.rb
@@ -16,49 +16,46 @@ class Day < ActiveRecord::Base
       @avg_value.nan? ? "N/A" : @avg_value
   end
   
-   def self.to_like_conditions( name, search )
-
-    array = search.split
-    like_conditions = []
-    key_count = array.size
-    k = ""
-    array.each do |value|
-      k += "#{name} LIKE ?"
-      if key_count > 1 
-        k += " or "
-      end
-      key_count -= 1
-    end
-    like_conditions << k
-
-    array.each do |value| 
-      like_conditions << "%#{value}%"
-    end
-	
-	like_conditions
-  end
   
    def self.search(search, city)
     if search
-	 #find(:all, :conditions => ['description LIKE ? and location LIKE ?', %search%, %city%])
-	
-	str = to_like_conditions('description', search)
-	  find(:all, :conditions => (str))
-	  #'description LIKE ? and location LIKE ?', "%#{city}%"])
-	 
-	#find (:all, :conditions => ['description like ?', #{search}])
-	#str)
-	
-	#:conditions => to_like_conditions(description, city))
-  
-	  
-	    #find (:all, :conditions => { :descrip => ["Bob", "Steve", "Fred"] }
-
-	 #"%#{search}%"], ['location LIKE ?', c]})
+	 find(:all, :conditions => build_search_query(search, city))
     else
       find(:all)
     end
   end
   
+  def self.build_search_query(search, city ) # description (LIKE '<term1>' OR '<term2>') AND location LIKE '<city>'
+
+    array = search.split
+    like_conditions = []
+    key_count = array.size
+    
+	#handle no terms
+	if key_count > 0
+		k = "("
+	else
+		k = ""
+	end
+	
+    array.each do |value|
+      k << "description LIKE '%%#{value}%%'"
+      if key_count > 1 
+        k += " or "
+      end
+      key_count -= 1
+    end
+    
+	#AND city query if terms
+	if array.size > 0
+      k += ") AND "
+	end
+	
+	k += "location LIKE '%%#{city}%%'"
+    like_conditions << k
+
+	puts like_conditions
+	like_conditions
+  end
 
 end
