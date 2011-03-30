6b11c79750239fa805aa1e886c51799e42972be9
Jacob Hartsoch <jacobhartsoch@yahoo.com>
2011-03-29 19:27:00 -0700

    added support for putting markers on day/index view

--- public/javascripts/geokit.js
+++ public/javascripts/geokit.js
@@ -1,24 +1,22 @@
 var map;
 
 $(function () {
-	$('#city').location_autocomplete();
+	$('#city').search_location_autocomplete();
 	});
 	
 
 $(function () {
-	$('#day_location').location_autocomplete();
+	$('#day_location').day_location_autocomplete();
 	});
 
-function initialize()
- {
+$(function () {
+
     if (GBrowserIsCompatible())
     {
         map = new GMap2(document.getElementById("map"));
         map.setCenter(new GLatLng(48.76152, -122.4978133), 13);
         map.setUIToDefault();
         map.enableGoogleBar();
-
-
 
         var myEventListener = GEvent.bind(map, "click", this,
         function(overlay, latlng)
@@ -32,9 +30,39 @@ function initialize()
 
 
     }
+});
+
+function add_marker(lat, lng)
+{
+	var point = new GLatLng(lat, lng);
+	map.addOverlay(new GMarker(point));
+	map.setCenter(new GLatLng(lat, lng), 12);
+};
+
+$.fn.day_location_autocomplete = function(settings) {
+    return this.each(function() {
+        console.log($(this));
+        $(this).autocomplete({
+            source: "/locations/",
+            delay: 500,
+            minLength: 2,
+            autoFocus: true,
+            select: function(event, ui) {		
+				$("#day_latitude").val(ui.item.lat);
+				$("#day_longitude").val(ui.item.lng);
+
+				add_marker(ui.item.lat, ui.item.lng);		          
+            },
+
+            open: function(event, ui) {
+                $('.ui-autocomplete').css('z-index', '9999');
+                $('.ui-autocomplete').css('width', $(event.target).outerWidth());
+            }
+        });
+    });
 }
 
-$.fn.location_autocomplete = function(settings) {
+$.fn.search_location_autocomplete = function(settings) {
     return this.each(function() {
         console.log($(this));
         $(this).autocomplete({
@@ -43,14 +71,7 @@ $.fn.location_autocomplete = function(settings) {
             minLength: 2,
             autoFocus: true,
             select: function(event, ui) {
-								$("#day_latitude").val(ui.item.lat);
-								$("#day_longitude").val(ui.item.lng);
-
-                var point = new GLatLng(ui.item.lat, ui.item.lng);
-                map.addOverlay(new GMarker(point));
-                map.setCenter(new GLatLng(ui.item.lat, ui.item.lng), 12);
-                
-          
+				add_marker(ui.item.lat, ui.item.lng);	
             },
 
             open: function(event, ui) {
