955a7787d7d1d5a5355b6885cff5562f512898a3
Jacob Hartsoch <jacobhartsoch@yahoo.com>
2011-04-04 14:49:57 -0700

    fixed comment routing, still issue with ratings I think

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,8 @@ class CommentsController < ApplicationController
   def create
     @day = Day.find(params[:post_id])
     @comment = @day.comments.create(params[:comment])
-    redirect_to post_path(@day)
+	@comment.user = current_user
+	@comment.save
+    redirect_to day_path(@day)
   end
 end
