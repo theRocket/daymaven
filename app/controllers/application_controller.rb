b42560a384235428d5264aa8864c8053e9a2ac57
Jacob Hartsoch <jacobhartsoch@yahoo.com>
2011-05-02 08:11:17 -0700

    Added 301 redirect code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :ensure_domain
+
+  protected
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != 'daymaven.com' && Rails.env.production?
+      redirect_to "http://daymaven.com", :status => 301
+    end  
+  end
+
 end
+
